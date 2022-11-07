8c2e9d2926ba251817b6dd3a4d2cba71e83ef318
John <johnr@tokyo.uk>
2022-11-07 11:14:27 +0000

    Adds tests for load-more.js

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,7 +1,10 @@
 import originalConfig from '../rollup.config';
 
 const testConfig = Object.assign({}, originalConfig(), {
-  input: ['site/_js/web-components/checkbox-group/_checkbox-group'],
+  input: [
+    'site/_js/web-components/checkbox-group/_checkbox-group',
+    'site/_js/misc/load-more/_load-more',
+  ],
 });
 
 export default testConfig;
